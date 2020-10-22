ce8ae6df0cd15451e127d0903e80c97d8a82a18d
TTT <52881509+TTT-824@users.noreply.github.com>
2020-10-23 01:05:55 +0800

    修改swagger-ui配置

--- Chapter10/src/main/java/com/ttt/config/SwaggerConfig.java
+++ Chapter10/src/main/java/com/ttt/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ package com.ttt.config;
 * swaggerconfig class
 * */
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -17,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiinfo())
